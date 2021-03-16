6e6fcb9489109a639d32ff0aa74047b0f134e70c
humulla <humairaa.mulla@hackney.gov.uk>
2021-03-16 15:41:10 +0000

    Correlation middleware complete, all test passing

--- PersonApi/V1/Controllers/BaseController.cs
+++ PersonApi/V1/Controllers/BaseController.cs
@@ -3,6 +3,7 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 using PersonApi.V1.Domain;
 using PersonApi.V1.Infrastructure;
+using System.Collections.Generic;
 
 namespace PersonApi.V1.Controllers
 {
@@ -16,7 +17,7 @@ namespace PersonApi.V1.Controllers
         public string GetCorrelationId()
         {
             if (HttpContext.Request.Headers[CorrelationConstants.CorrelationId].Count == 0)
-                throw new CorrelationNotFoundException("Request is missing a correlationId");
+                throw new KeyNotFoundException("Request is missing a correlationId");
 
             return HttpContext.Request.Headers[CorrelationConstants.CorrelationId];
         }
