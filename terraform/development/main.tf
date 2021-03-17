882c9ebd8bff0a0add732de2954a6f224be0c73a
Mehdi Kimakhe <mehdi.kimakhe@amido.com>
2021-03-17 14:58:31 +0000

    MTTL-176 - Make deployment dependant on terraform step.

--- terraform/development/main.tf
+++ terraform/development/main.tf
@@ -12,6 +12,14 @@ provider "aws" {
   version = "~> 2.0"
 }
 
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
+
+locals {
+    parameter_store = "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter"
+}
+
 terraform {
   backend "s3" {
     bucket  = "terraform-state-tenants-and-leaseholders-dev"
