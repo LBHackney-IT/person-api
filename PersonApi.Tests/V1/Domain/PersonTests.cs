206b0d5ec35080f96b97287525c524dc23312086
mikemoody-amido <77778488+mikemoody-amido@users.noreply.github.com>
2021-03-25 17:23:15 +0000

    Updated variable name

--- PersonApi.Tests/V1/Domain/PersonTests.cs
+++ PersonApi.Tests/V1/Domain/PersonTests.cs
@@ -9,38 +9,37 @@ namespace PersonApi.Tests.V1.Domain
     [TestFixture]
     public class PersonTests
     {
-
         [Test]
         public void PersonHasPropertiesSet()
         {
-            Person entity = Constants.ConstructPersonFromConstants();
+            Person person = Constants.ConstructPersonFromConstants();
 
-            entity.Id.Should().Be(Constants.ID);
-            entity.Title.Should().Be(Constants.TITLE);
-            entity.PreferredFirstname.Should().Be(Constants.PREFIRSTNAME);
-            entity.PreferredSurname.Should().Be(Constants.PREFSURNAME);
-            entity.Firstname.Should().Be(Constants.FIRSTNAME);
-            entity.MiddleName.Should().Be(Constants.MIDDLENAME);
-            entity.Surname.Should().Be(Constants.SURNAME);
-            entity.Ethinicity.Should().Be(Constants.ETHNICITY);
-            entity.Nationality.Should().Be(Constants.NATIONALITY);
-            entity.PlaceOfBirth.Should().Be(Constants.PLACEOFBIRTH);
-            entity.DateOfBirth.Should().Be(Constants.DATEOFBIRTH);
-            entity.Gender.Should().Be(Constants.GENDER);
-            entity.Identifications.Should().ContainSingle();
-            entity.Identifications.First().IdentificationType.Should().Be(Constants.IDENTIFICATIONTYPE);
-            entity.Identifications.First().IsOriginalDocumentSeen.Should().Be(Constants.IDENTIFICATIONISSEEN);
-            entity.Identifications.First().LinkToDocument.Should().Be(Constants.IDENTIFICATIONLINKTODOC);
-            entity.Identifications.First().Value.Should().Be(Constants.IDENTIFICATIONVALUE);
-            entity.Languages.Should().ContainSingle();
-            entity.Languages.First().IsPrimary.Should().Be(Constants.LANGUAGEISPRIMARY);
-            entity.Languages.First().Name.Should().Be(Constants.LANGUAGENAME);
-            entity.CommunicationRequirements.Should().BeEquivalentTo(Constants.COMMSREQ);
-            entity.PersonTypes.Should().BeEquivalentTo(Constants.PERSONTYPES);
-            entity.Links.Should().ContainSingle();
-            entity.Links.First().EndpointType.Should().Be(Constants.LINKENDPOINTTYPE);
-            entity.Links.First().Href.Should().Be(Constants.LINKHREF);
-            entity.Links.First().Rel.Should().Be(Constants.LINKREL);
+            person.Id.Should().Be(Constants.ID);
+            person.Title.Should().Be(Constants.TITLE);
+            person.PreferredFirstname.Should().Be(Constants.PREFIRSTNAME);
+            person.PreferredSurname.Should().Be(Constants.PREFSURNAME);
+            person.Firstname.Should().Be(Constants.FIRSTNAME);
+            person.MiddleName.Should().Be(Constants.MIDDLENAME);
+            person.Surname.Should().Be(Constants.SURNAME);
+            person.Ethinicity.Should().Be(Constants.ETHNICITY);
+            person.Nationality.Should().Be(Constants.NATIONALITY);
+            person.PlaceOfBirth.Should().Be(Constants.PLACEOFBIRTH);
+            person.DateOfBirth.Should().Be(Constants.DATEOFBIRTH);
+            person.Gender.Should().Be(Constants.GENDER);
+            person.Identifications.Should().ContainSingle();
+            person.Identifications.First().IdentificationType.Should().Be(Constants.IDENTIFICATIONTYPE);
+            person.Identifications.First().IsOriginalDocumentSeen.Should().Be(Constants.IDENTIFICATIONISSEEN);
+            person.Identifications.First().LinkToDocument.Should().Be(Constants.IDENTIFICATIONLINKTODOC);
+            person.Identifications.First().Value.Should().Be(Constants.IDENTIFICATIONVALUE);
+            person.Languages.Should().ContainSingle();
+            person.Languages.First().IsPrimary.Should().Be(Constants.LANGUAGEISPRIMARY);
+            person.Languages.First().Name.Should().Be(Constants.LANGUAGENAME);
+            person.CommunicationRequirements.Should().BeEquivalentTo(Constants.COMMSREQ);
+            person.PersonTypes.Should().BeEquivalentTo(Constants.PERSONTYPES);
+            person.Links.Should().ContainSingle();
+            person.Links.First().EndpointType.Should().Be(Constants.LINKENDPOINTTYPE);
+            person.Links.First().Href.Should().Be(Constants.LINKHREF);
+            person.Links.First().Rel.Should().Be(Constants.LINKREL);
         }
     }
 }
