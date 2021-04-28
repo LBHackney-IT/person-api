c57e28516bcf7da81901560cd7a98d8146695d8d
mikemoody-amido <77778488+mikemoody-amido@users.noreply.github.com>
2021-04-28 17:16:48 +0100

    Functional gateway tests now use real DynamoDb instance (#14)
    * Functional gateway tests now use real DynamoDb instance
    
    * Add generic cleanup to gateway tests.

--- PersonApi.Tests/V1/Gateways/DynamoDbGatewayTests.cs
+++ PersonApi.Tests/V1/Gateways/DynamoDbGatewayTests.cs
@@ -8,24 +8,45 @@ using PersonApi.V1.Factories;
 using PersonApi.V1.Gateways;
 using PersonApi.V1.Infrastructure;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Xunit;
 
 namespace PersonApi.Tests.V1.Gateways
 {
-    [Collection("LogCall collection")]
-    public class DynamoDbGatewayTests
+    [Collection("DynamoDb collection")]
+    public class DynamoDbGatewayTests : IDisposable
     {
         private readonly Fixture _fixture = new Fixture();
-        private readonly Mock<IDynamoDBContext> _dynamoDb;
         private readonly Mock<ILogger<DynamoDbGateway>> _logger;
-        private readonly DynamoDbGateway _classUnderTest;
+        private DynamoDbGateway _classUnderTest;
 
-        public DynamoDbGatewayTests()
+        private readonly IDynamoDBContext _dynamoDb;
+        private readonly List<Action> _cleanup = new List<Action>();
+
+        public DynamoDbGatewayTests(DynamoDbIntegrationTests<Startup> dbTestFixture)
         {
-            _dynamoDb = new Mock<IDynamoDBContext>();
+            _dynamoDb = dbTestFixture.DynamoDbContext;
             _logger = new Mock<ILogger<DynamoDbGateway>>();
-            _classUnderTest = new DynamoDbGateway(_dynamoDb.Object, _logger.Object);
+            _classUnderTest = new DynamoDbGateway(_dynamoDb, _logger.Object);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private bool _disposed;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing && !_disposed)
+            {
+                foreach (var action in _cleanup)
+                    action();
+
+                _disposed = true;
+            }
         }
 
         [Fact]
@@ -44,19 +65,19 @@ namespace PersonApi.Tests.V1.Gateways
         public async Task GetPersonByIdReturnsThePersonIfItExists()
         {
             // Arrange
-            var entity = _fixture.Create<Person>();
+            var entity = _fixture.Build<Person>()
+                                 .With(x => x.DateOfBirth, DateTime.UtcNow.AddYears(-30))
+                                 .Create();
             var dbEntity = entity.ToDatabase();
-            var dbIdUsed = entity.Id;
 
-            _dynamoDb.Setup(x => x.LoadAsync<PersonDbEntity>(dbIdUsed, default))
-                     .ReturnsAsync(dbEntity);
+            await _dynamoDb.SaveAsync(dbEntity).ConfigureAwait(false);
+            _cleanup.Add(async () => await _dynamoDb.DeleteAsync(dbEntity).ConfigureAwait(false));
 
             // Act
             var response = await _classUnderTest.GetPersonByIdAsync(entity.Id).ConfigureAwait(false);
 
             // Assert
-            _dynamoDb.Verify(x => x.LoadAsync<PersonDbEntity>(dbIdUsed, default), Times.Once);
-            entity.Id.Should().Be(response.Id);
+            entity.Should().BeEquivalentTo(response);
             _logger.VerifyExact(LogLevel.Debug, $"Calling IDynamoDBContext.LoadAsync for id {entity.Id}", Times.Once());
         }
 
@@ -64,17 +85,19 @@ namespace PersonApi.Tests.V1.Gateways
         public void GetPersonByIdExceptionThrow()
         {
             // Arrange
+            var mockDynamoDb = new Mock<IDynamoDBContext>();
+            _classUnderTest = new DynamoDbGateway(mockDynamoDb.Object, _logger.Object);
             var id = Guid.NewGuid();
             var exception = new ApplicationException("Test exception");
-            _dynamoDb.Setup(x => x.LoadAsync<PersonDbEntity>(id, default))
-                     .ThrowsAsync(exception);
+            mockDynamoDb.Setup(x => x.LoadAsync<PersonDbEntity>(id, default))
+                        .ThrowsAsync(exception);
 
             // Act
             Func<Task<Person>> func = async () => await _classUnderTest.GetPersonByIdAsync(id).ConfigureAwait(false);
 
             // Assert
             func.Should().Throw<ApplicationException>().WithMessage(exception.Message);
-            _dynamoDb.Verify(x => x.LoadAsync<PersonDbEntity>(id, default), Times.Once);
+            mockDynamoDb.Verify(x => x.LoadAsync<PersonDbEntity>(id, default), Times.Once);
             _logger.VerifyExact(LogLevel.Debug, $"Calling IDynamoDBContext.LoadAsync for id {id}", Times.Once());
         }
     }
