bef62b35ab451ffbcc51ccbe15897e71fb8e8c05
mikemoody-amido <77778488+mikemoody-amido@users.noreply.github.com>
2021-05-12 12:33:43 +0100

    Move dynamodb role to the serverless.yaml

--- terraform/development/dynamodb.tf
+++ terraform/development/dynamodb.tf
@@ -17,33 +17,3 @@ resource "aws_dynamodb_table" "personapi_dynamodb_table" {
         project_name      = var.project_name
     }
 }
-
-resource "aws_iam_policy" "personapi_dynamodb_table_policy" {
-    name                  = "lambda-dynamodb-person-api"
-    description           = "A policy allowing read/write operations on person dynamoDB for the person API"
-    path                  = "/person-api/"
-
-    policy                = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                        "dynamodb:BatchGet*",
-                        "dynamodb:BatchWrite*",
-                        "dynamodb:DeleteItem",
-                        "dynamodb:DescribeStream",
-                        "dynamodb:DescribeTable",
-                        "dynamodb:Get*",
-                        "dynamodb:PutItem",
-                        "dynamodb:Query",
-                        "dynamodb:Scan",
-                        "dynamodb:UpdateItem"
-                     ],
-            "Resource": "${aws_dynamodb_table.personapi_dynamodb_table.arn}"
-        }
-    ]
-}
-EOF
-}
