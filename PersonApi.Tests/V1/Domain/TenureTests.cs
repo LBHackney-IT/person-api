1087a0227c9f32c313c1d8965789dec8e85d3c61
Evangelos Aktoudianakis <evangelos.aktoudianakis@amido.com>
2021-05-14 16:16:25 +0100

    Added extra property + tests

---
+++ PersonApi.Tests/V1/Domain/TenureTests.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Globalization;
+using FluentAssertions;
+using PersonApi.V1.Domain;
+using Xunit;
+
+namespace PersonApi.Tests.V1.Domain
+{
+    public class TenureTests
+    {
+        private Tenure _classUnderTest;
+
+        public TenureTests()
+        {
+            _classUnderTest = new Tenure();
+        }
+
+        [Fact]
+        public void GivenATenureWhenEndDateIsNullThenIsActiveShouldBeTrue()
+        {
+            // given + when + then
+            _classUnderTest.IsActive.Should().BeTrue();
+        }
+
+        [Fact]
+        public void GivenATenureWhenEndDateIsGreaterThanCurrentDateThenIsActiveShouldBeTrue()
+        {
+            // given
+            _classUnderTest.EndDate = DateTime.Now.AddDays(1).ToShortDateString();
+            
+            // when + then
+            _classUnderTest.IsActive.Should().BeTrue();
+        }
+
+        [Fact]
+        public void GivenATenureWhenEndDateIsLessThanCurrentDateThenIsActiveShouldBeFalse()
+        {
+            // given
+            _classUnderTest.EndDate = DateTime.Now.AddDays(-1).ToShortDateString();
+
+            // when + then
+            _classUnderTest.IsActive.Should().BeFalse();
+        }
+    }
+}
