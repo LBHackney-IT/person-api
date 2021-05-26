56c5f9e875d258941e311012d5e6d8af1b15d337
mikemoody-amido <77778488+mikemoody-amido@users.noreply.github.com>
2021-05-26 11:45:09 +0100

    Formatting

--- PersonApi.Tests/V1/Helper/Constants.cs
+++ PersonApi.Tests/V1/Helper/Constants.cs
@@ -33,7 +33,7 @@ namespace PersonApi.Tests.V1.Helper
         public const string ASSETFULLADDRESS = "SomeAddress";
         public const string STARTDATE = "2012-07-19";
         public const string ENDDATE = "2015-07-19";
-                   
+
         public static IEnumerable<CommunicationRequirement> COMMSREQ { get; }
             = new List<CommunicationRequirement> { CommunicationRequirement.SignLanguage };
         public static IEnumerable<PersonType> PERSONTYPES { get; }
