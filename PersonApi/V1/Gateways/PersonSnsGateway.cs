7c90944db82c5ffbf3569149cc44355be56f84e3
Evangelos Aktoudianakis <evangelos.aktoudianakis@amido.com>
2021-06-02 17:05:00 +0100

    Trying to figure out why this is not working

--- PersonApi/V1/Gateways/PersonSnsGateway.cs
+++ PersonApi/V1/Gateways/PersonSnsGateway.cs
@@ -2,6 +2,7 @@ using System;
 using System.Threading.Tasks;
 using Amazon.SimpleNotificationService;
 using Amazon.SimpleNotificationService.Model;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
 using PersonApi.V1.Domain;
@@ -14,17 +15,21 @@ namespace PersonApi.V1.Gateways
     {
         private readonly IAmazonSimpleNotificationService _amazonSimpleNotificationService;
         private readonly IOptions<AwsConfiguration> _settings;
+        private readonly ILogger<PersonSnsGateway> _logger;
 
-        public PersonSnsGateway(IAmazonSimpleNotificationService amazonSimpleNotificationService, IOptions<AwsConfiguration> settings)
+        public PersonSnsGateway(IAmazonSimpleNotificationService amazonSimpleNotificationService, IOptions<AwsConfiguration> settings, ILogger<PersonSnsGateway> logger)
         {
             _amazonSimpleNotificationService = amazonSimpleNotificationService;
             _settings = settings;
+            _logger = logger;
         }
 
         [LogCall]
         public async Task Publish(PersonSns personSns)
         {
             string message = JsonConvert.SerializeObject(personSns);
+            _logger.Log(LogLevel.Information, Environment.GetEnvironmentVariable("DynamoDb_LocalMode"));
+            _logger.Log(LogLevel.Information, Environment.GetEnvironmentVariable("Localstack_SnsServiceUrl"));
 
             var request = new PublishRequest { Message = message, TopicArn = Environment.GetEnvironmentVariable("PersonTopicArn") };
             await _amazonSimpleNotificationService.PublishAsync(request).ConfigureAwait(false);
