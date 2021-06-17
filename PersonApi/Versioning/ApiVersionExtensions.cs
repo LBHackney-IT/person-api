9e1b2c33de361ddf486f5620e462631d18c5b498
mikemoody-amido <77778488+mikemoody-amido@users.noreply.github.com>
2021-06-17 15:24:13 +0100

    Minor changes to improve test coverage

--- PersonApi/Versioning/ApiVersionExtensions.cs
+++ PersonApi/Versioning/ApiVersionExtensions.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.CodeAnalysis;
 
 namespace PersonApi.Versioning
 {
+    [ExcludeFromCodeCoverage]
     public static class ApiVersionExtensions
     {
         public static string GetFormattedApiVersion(this ApiVersion apiVersion)
