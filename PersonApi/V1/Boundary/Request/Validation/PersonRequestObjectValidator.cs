8392538e882ef08bbe53cc11c180e3eec68ba1fd
Evangelos Aktoudianakis <evangelos.aktoudianakis@amido.com>
2021-07-02 14:35:06 +0100

    Trying to make Gender optional.

--- PersonApi/V1/Boundary/Request/Validation/PersonRequestObjectValidator.cs
+++ PersonApi/V1/Boundary/Request/Validation/PersonRequestObjectValidator.cs
@@ -29,8 +29,7 @@ namespace PersonApi.V1.Boundary.Request.Validation
             RuleFor(x => x.Surname).NotNull()
                                    .NotEmpty()
                                    .NotXssString();
-            RuleFor(x => x.Gender).NotNull()
-                                  .IsInEnum();
+            
             RuleFor(x => x.NationalInsuranceNo)
                                  .Matches(NiRegEx, RegexOptions.IgnoreCase)
                                  .When(x => !string.IsNullOrEmpty(x.NationalInsuranceNo));
