6c2c9f93045291a160930af12c48f72eabf76840
Evangelos Aktoudianakis <evangelos.aktoudianakis@amido.com>
2021-07-15 11:39:57 +0100

    Added validation tests

--- PersonApi.Tests/V1/Boundary/Validation/TenureValidatorTests.cs
+++ PersonApi.Tests/V1/Boundary/Validation/TenureValidatorTests.cs
@@ -126,9 +126,11 @@ namespace PersonApi.Tests.V1.Boundary.Request.Validation
         [InlineData("Some string with <tag> in it.")]
         public void UprnShouldErrorWithInvalidValue(string invalid)
         {
-            var model = new Tenure() { Uprn = invalid };
+            var model = new Tenure() { Uprn = invalid, PaymentReference = invalid, PropertyReference = invalid};
             var result = _sut.TestValidate(model);
             result.ShouldHaveValidationErrorFor(x => x.Uprn);
+            result.ShouldHaveValidationErrorFor(x => x.PropertyReference);
+            result.ShouldHaveValidationErrorFor(x => x.PaymentReference);
         }
 
         [Theory]
