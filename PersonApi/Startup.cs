4e316619253683c4f749dfbb97184b4174c9d871
Evangelos Aktoudianakis <evangelos.aktoudianakis@amido.com>
2021-07-21 12:25:02 +0100

    Trying to add headers in .NET Startup

--- PersonApi/Startup.cs
+++ PersonApi/Startup.cs
@@ -38,6 +38,7 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text.Json.Serialization;
+using Microsoft.Net.Http.Headers;
 
 namespace PersonApi
 {
@@ -59,7 +60,14 @@ namespace PersonApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddCors();
+            services.AddCors(options => options.AddPolicy("ApiCorsPolicy", builder =>
+            {
+                builder
+                    .AllowAnyOrigin()
+                    .WithHeaders(HeaderNames.ContentType, HeaderNames.Authorization, "If-Match", "ETag")
+                    .AllowAnyMethod()
+                    .AllowCredentials();
+            }));
 
             services
                 .AddMvc()
@@ -178,11 +186,7 @@ namespace PersonApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public static void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
         {
-            app.UseCors(builder => builder
-                .AllowAnyOrigin()
-                .AllowAnyHeader()
-                .AllowAnyMethod()
-                .WithExposedHeaders("ETag", "If-Match"));
+            app.UseCors("ApiCorsPolicy");
 
             if (env.IsDevelopment())
             {
