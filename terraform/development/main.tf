fc4ed295043359059795a5dc270e7dcdaee2e04e
humulla <humairaa.mulla@hackney.gov.uk>
2021-08-11 11:18:48 +0100

    add s for duration

--- terraform/development/main.tf
+++ terraform/development/main.tf
@@ -52,7 +52,7 @@ resource "aws_ssm_parameter" "person_sns_arn" {
   value = aws_sns_topic.person_topic.arn
 }
 
-resource "aws_synthetics_canary" "canaries" {
+resource "aws_synthetics_canary" "health_check_canaries" {
   name                 = "person-health-check"
   artifact_s3_location = "s3://cw-syn-results-${data.aws_caller_identity.current.account_id}/canary/-${data.aws_region.current.name}/"
   execution_role_arn   = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/LBH_Canary_Role"
@@ -60,7 +60,7 @@ resource "aws_synthetics_canary" "canaries" {
   runtime_version      = "syn-nodejs-puppeteer-3.0"
 
   schedule {
-    expression = "rate(5 minute)"
+    expression = "rate(5 minutes)"
   }
 }
 
