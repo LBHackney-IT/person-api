029a0d6f76be59ad6d360101786e4a1e7c556ae1
Callum Macpherson <callum.macphers@hackney.gov.uk>
2021-09-02 11:04:45 +0100

    Sorted order of tenures in PersonResponseObject.

--- PersonApi.Tests/V1/Factories/ResponseFactoryTest.cs
+++ PersonApi.Tests/V1/Factories/ResponseFactoryTest.cs
@@ -5,6 +5,7 @@ using PersonApi.V1.Boundary;
 using PersonApi.V1.Domain;
 using PersonApi.V1.Factories;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Xunit;
 
@@ -15,6 +16,8 @@ namespace PersonApi.Tests.V1.Factories
         private readonly Fixture _fixture = new Fixture();
         private readonly Mock<IApiLinkGenerator> _mockLinkGenerator;
         private readonly ResponseFactory _sut;
+
+        private readonly Random _random = new Random();
 
         public ResponseFactoryTest()
         {
@@ -66,6 +69,79 @@ namespace PersonApi.Tests.V1.Factories
             response.DateOfBirth.Should().Be(ResponseFactory.FormatDateOfBirth(person.DateOfBirth));
             response.PersonTypes.Should().BeEquivalentTo(person.PersonTypes);
             response.Tenures.Should().BeEquivalentTo(person.Tenures?.Select(x => ResponseFactory.ToResponse(x)));
+        }
+
+
+        // Test that tenures are ordered
+        // Active first - newest to oldest
+        // Inactive - newest to oldest
+
+        // create list of active tenures - random dates
+        // create list of inactive tenures - random dates
+
+        [Fact]
+        public void PersonResponseObjectToResponseWhenCalledReturnsTenuresInCorrectOrder()
+        {
+            var numberOfActiveTenures = _random.Next(2, 5);
+            var numberOfInactiveTenures = _random.Next(2, 5);
+
+            // create random list of active and inactive tenures
+            var shuffledTenures = CreateListOfShuffledTenures(numberOfActiveTenures, numberOfInactiveTenures);
+
+            // mock person
+            var mockPerson = _fixture.Build<Person>().With(x => x.Tenures, shuffledTenures).Create();
+
+            // call method
+            var response = _sut.ToResponse(mockPerson);
+
+            // seperate response into what should be active and inactive tenures
+            var responseActiveTenures = response.Tenures.Take(numberOfActiveTenures);
+            var responseInactiveTenures = response.Tenures.Skip(numberOfActiveTenures).Take(numberOfInactiveTenures);
+
+
+            // assert first half of tenures are active
+            responseActiveTenures.Should().OnlyContain(x => x.IsActive == true);
+
+            // assert second half of tenures are inactive
+            responseInactiveTenures.Should().OnlyContain(x => x.IsActive == false);
+
+            // assert first half of tenures is in date order
+            responseActiveTenures.Select(x => DateTime.Parse(x.StartDate)).Should().BeInDescendingOrder();
+
+            // assert second half of tenures is in date order
+            responseInactiveTenures.Select(x => DateTime.Parse(x.StartDate)).Should().BeInDescendingOrder();
+        }
+
+        private List<Tenure> CreateListOfShuffledTenures(int numberOfActiveTenures, int numberOfInactiveTenures)
+        {
+            // Tenure.IsActive is readonly. Must set enddate to null or date in past
+            string activeTenureDateValue = null;
+            string inactiveTeunureDateValue = DateTime.UtcNow.AddDays(-7).ToString(); // generate date in past
+
+            // create list of active tenures - random dates
+            var activeTenures = _fixture.Build<Tenure>()
+                .With(x => x.EndDate, activeTenureDateValue)
+                .With(x => x.StartDate, CreateRandomStartDateValue)
+                .CreateMany(numberOfActiveTenures);
+
+            // create list of inactive tenures - random dates
+            var inactiveTenures = _fixture.Build<Tenure>()
+                .With(x => x.EndDate, inactiveTeunureDateValue)
+                .With(x => x.StartDate, CreateRandomStartDateValue)
+                .CreateMany(numberOfInactiveTenures);
+
+            // combine shuffle list
+            var shuffledTenures = activeTenures.Concat(inactiveTenures).OrderBy(item => _random.Next());
+
+            return shuffledTenures.ToList();
+        }
+
+        private string CreateRandomStartDateValue()
+        {
+            // An inactive tenure must have enddate set in past
+            var numberOfDaysInPast = _random.Next(-1000, -1);
+
+            return DateTime.UtcNow.AddDays(numberOfDaysInPast).ToString();
         }
     }
 }
--- PersonApi/V1/Factories/ResponseFactory.cs
+++ PersonApi/V1/Factories/ResponseFactory.cs
@@ -2,6 +2,7 @@ using PersonApi.V1.Boundary;
 using PersonApi.V1.Boundary.Response;
 using PersonApi.V1.Domain;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace PersonApi.V1.Factories
@@ -38,9 +39,27 @@ namespace PersonApi.V1.Factories
                 DateOfBirth = FormatDateOfBirth(domain.DateOfBirth),
                 PersonTypes = domain.PersonTypes,
                 Links = _apiLinkGenerator?.GenerateLinksForPerson(domain),
-                Tenures = domain.Tenures?.Select(x => ToResponse(x)).ToList(),
+                //Tenures = domain.Tenures?.Select(x => ToResponse(x)).ToList(),
+                Tenures = SortTentures(domain.Tenures),
                 Reason = domain.Reason
             };
+        }
+
+        private static List<TenureResponseObject> SortTentures(IEnumerable<Tenure> tenures)
+        {
+            if (tenures == null) return null;
+
+            // 1. Filter and order active tenures - newest to oldest
+            var activeTenures = tenures.Where(x => x.IsActive).OrderByDescending(x => DateTime.Parse(x.StartDate));
+
+            // 2. Filter and order inactive tenures - newest to oldest
+            var inactiveTenures = tenures.Where(x => x.IsActive == false).OrderByDescending(x => DateTime.Parse(x.StartDate));
+
+            // 3. create combined list of all tenures
+            var allTenures = activeTenures.Concat(inactiveTenures);
+
+            // 4. convert all to TenureResponseObject
+            return allTenures.ToList().Select(x => ToResponse(x)).ToList();
         }
 
         public static TenureResponseObject ToResponse(Tenure tenure)
