1e2ef4b6ccf34eb8c5c48a9f29d0c05bcb24a241
Charnjit Sohal <40811385+charnjitsohal1@users.noreply.github.com>
2021-09-03 13:26:24 +0100

    added cloudwatch dashboard TF module (#104) (#105)
    * added cloudwatch dashboard TF module
    
    * updated tags

--- terraform/production/dynamodb.tf
+++ terraform/production/dynamodb.tf
@@ -1,24 +1,21 @@
 resource "aws_dynamodb_table" "personapi_dynamodb_table" {
-    name                  = "Persons"
-    billing_mode          = "PROVISIONED"
-    read_capacity         = 10
-    write_capacity        = 10
-    hash_key              = "id"
+  name           = "Persons"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 10
+  write_capacity = 10
+  hash_key       = "id"
 
-    attribute {
-        name              = "id"
-        type              = "S"
-    }
+  attribute {
+    name = "id"
+    type = "S"
+  }
 
-    tags = {
-        Name              = "person-api-${var.environment_name}"
-        Environment       = var.environment_name
-        terraform-managed = true
-        project_name      = var.project_name
-        BackupPolicy     = "Prod"
-    }
+  tags = merge(
+    local.default_tags,
+    { BackupPolicy = "Prod" }
+  )
 
-    point_in_time_recovery {
-        enabled           = true
-    }
+  point_in_time_recovery {
+    enabled = true
+  }
 }
