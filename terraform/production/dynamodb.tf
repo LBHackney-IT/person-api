3e794815a3e148b7fe79eb24d90c218fef8b50a7
Callum Macpherson <88662046+LBHCallumM@users.noreply.github.com>
2024-02-07 11:59:10 +0000

    Change dynamodb to on-demand capacity (#159)
    * Change dynamodb to on-demand capacity
    
    * Update sonarcloud version
    
    * Upgrade java
    
    * Disable test

--- terraform/production/dynamodb.tf
+++ terraform/production/dynamodb.tf
@@ -1,8 +1,6 @@
 resource "aws_dynamodb_table" "personapi_dynamodb_table" {
   name           = "Persons"
-  billing_mode   = "PROVISIONED"
-  read_capacity  = 10
-  write_capacity = 10
+  billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "id"
 
   attribute {
