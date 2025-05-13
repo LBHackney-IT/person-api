ac7f4dabd43481ea0729ad600e3a19aeb17df531
humulla <43747248+humulla@users.noreply.github.com>
2025-05-13 09:08:14 +0100

    stg and prod terraform

--- terraform/production/dynamodb.tf
+++ terraform/production/dynamodb.tf
@@ -17,3 +17,23 @@ resource "aws_dynamodb_table" "personapi_dynamodb_table" {
     enabled = true
   }
 }
+
+resource "aws_dynamodb_table" "refGenerator_dynamodb_table" {
+  name           = "RefGenerator"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "refName"
+
+  attribute {
+    name = "refName"
+    type = "S"
+  }
+
+  tags = merge(
+    local.default_tags,
+    { BackupPolicy = "Prod" }
+  )
+
+  point_in_time_recovery {
+    enabled = true
+  }
+}
