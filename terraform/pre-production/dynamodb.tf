700fca5881493e25b64ebf6d897ee26b4aaf6dd3
LBHTKarki <tuomo.karki@hackney.gov.uk>
2025-06-09 12:45:58 +0100

    add refgenerator table

--- terraform/pre-production/dynamodb.tf
+++ terraform/pre-production/dynamodb.tf
@@ -19,3 +19,25 @@ resource "aws_dynamodb_table" "personapi_dynamodb_table" {
     enabled = false
   }
 }
+
+resource "aws_dynamodb_table" "refGenerator_dynamodb_table" {
+  name         = "RefGenerator"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "refName"
+
+  attribute {
+    name = "refName"
+    type = "S"
+  }
+
+  tags = merge(
+    local.default_tags,
+    {
+      BackupPolicy = "Dev", Backup = false, Confidentiality = "Internal"
+    }
+  )
+
+  point_in_time_recovery {
+    enabled = true
+  }
+}
